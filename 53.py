61e56b294bba9043a54b8dc32f943df2f9b51dda
Xiaowei Hu <67345731+Pergamono@users.noreply.github.com>
2021-07-16 23:50:23 -0500

    Update 53.py

--- 53.py
+++ 53.py
@@ -1,6 +1,6 @@
 # -*- coding: utf-8 -*-
 """
-53. Maximum Subarray
+53. Maximum Subarray  
 
 Given an integer array nums, find the contiguous subarray (containing at least one number) which has the largest sum and return its sum.
 """
